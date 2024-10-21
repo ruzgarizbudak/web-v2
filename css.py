32465ad9c4b322c1119501aac42e25c785a7aa49
ruzgarizbudak <izbudakruzgar@gmail.com>
2024-10-21 21:21:44 +0300

    Create css.py

---
+++ css.py
@@ -0,0 +1,156 @@
+
+@keyframes arkarenk{
+    0%{
+        background-color: black;
+    }
+    50%{
+        background-color: rgb(21, 21, 21);
+    }
+    100%{
+        background-color: rgb(4, 61, 38);
+    }
+}
+
+@keyframes yazi{
+    0%{
+        font-size: 15px;
+    }
+    25%{
+        font-size: 19px;
+    }
+    50%{
+        font-size: 21px
+    }
+    75%{
+        font-size: 19px;
+    }
+    100%{
+        font-size: 11px;
+    }
+}
+
+
+
+
+
+
+
+body{
+    color: antiquewhite;
+    font-size: 15px;
+    
+    
+}
+body{
+    animation: yazi 7.5s infinite;
+}
+
+h1{
+    text-align: center;
+    color: brown;
+    animation: arkarenk 4.5s infinite ;
+}
+.x{
+    color: green;
+}
+
+.q{
+    color: rgb(55, 55, 1);
+}
+
+.w{
+    color: rgb(17, 255, 0);
+}
+
+.c{
+    color: rgb(150, 24, 24);
+}
+
+.m{
+    color: silver;
+}
+
+.p{
+    color: goldenrod;
+}
+
+.er{
+    color: lightblue;
+}
+
+.h{
+    color: rgba(125, 0, 0, 0.838);
+}
+
+.b{
+    color: rgb(203, 25, 25);
+}
+
+.ah{
+    color: rgb(255, 147, 147);
+}
+
+.n{
+    color: rgb(118, 114, 114);
+}
+
+.tr{
+    color: rgb(85, 10, 183);
+}
+
+.hr{
+    color: rgb(6, 145, 145);
+}
+
+.wz{
+    color: rgb(91, 23, 2);
+}
+
+.dp{
+    color: #ad1e01c9;
+}
+
+h3{
+    color: blueviolet;
+}
+
+
+
+.th{
+    color: #019cadc9;
+}
+
+.dn{
+    color: goldenrod;
+}
+
+.tw{
+    color: rgb(5, 0, 102);
+}
+
+.la{
+    color: aquamarine;
+}
+
+.dm{
+    color: rgb(0, 41, 41);
+}
+
+
+@keyframes renk{
+    0%{
+        color: blue;
+    }
+    50%{
+        color: rgb(137, 21, 214);
+    }
+    100%{
+        color: rgb(55, 0, 0);
+    }
+}
+h3{
+    animation: renk 3s infinite;
+}
+
+
+
